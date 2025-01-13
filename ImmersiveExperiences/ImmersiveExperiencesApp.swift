6775076303f69deb71b1dd661e6ef32a8b8d3b7f
khoanisdev <doanvankhoan124@gmail.com>
2025-01-13 21:09:02 +0700

    Initial Commit

---
+++ ImmersiveExperiences/ImmersiveExperiencesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImmersiveExperiencesApp.swift
+//  ImmersiveExperiences
+//
+//  Created by Đoàn Văn Khoan on 13/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ImmersiveExperiencesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
